f230906cc49836f1a5c6a8ed4810faec172024ae
Vadim Lega <legavv@gmail.com>
2022-01-24 02:53:38 +0300

    feat: add burger menu

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,1 +1,8 @@
 console.log ("Вёрстка валидная +10 \nВёрстка семантическая +20 \nВёрстка соответствует макету +48 \nТребования к css +12 \nИнтерактивность, реализуемая через css +20 \nИТОГО: +110");
+const burger = document.querySelector('.burger');
+const navList = document.querySelector('.nav-list'); 
+const burgerLine = document.querySelectorAll('.burger-line');
+burger.addEventListener('click', () => {
+    navList.classList.toggle('active');
+    burger.classList.toggle('active');
+});
