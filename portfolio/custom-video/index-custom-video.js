671bbe79583f1c659c7074b3e28a8dab9a5ee6b8
Vadim Lega <legavv@gmail.com>
2022-02-06 13:58:32 +0300

    feat: add mute in pause and refactor code

--- portfolio/custom-video/index-custom-video.js
+++ portfolio/custom-video/index-custom-video.js
@@ -2,10 +2,10 @@ const videoPlayer = document.querySelector('.video-player')
 const playerViewer = document.querySelector('.player__viewer');
 const videoBtn = document.querySelector('.video-btn');
 const splashScreen = document.querySelector('.splash-screen');
-const playerPlayPause = document.querySelector('.player__play-pause');
-const play = document.querySelector('.player__play-pause')
-const pause = document.querySelector('.player__play-pause.pause')
-const mute = document.querySelector('.player__mute')
+const playPauseButton = document.querySelector('.player__button-play-pause');
+const play = document.querySelector('.player__button-play-pause')
+const pause = document.querySelector('.player__button-play-pause.pause')
+const muteButton = document.querySelector('.player__button-mute')
 const playerVolume = document.querySelector('.player__volume')
 const playerProgress = document.querySelector('.player__progress')
 let playPercent;
@@ -15,11 +15,11 @@ function togglePlay () {
     if (playerViewer.paused) {
     playerViewer.play()
     playerViewer.volume = playerVolume.value;
-    playerPlayPause.classList.remove('pause');
+    playPauseButton.classList.remove('pause');
     videoBtn.style.display = 'none';
     } else {
     playerViewer.pause();
-    playerPlayPause.classList.add('pause');
+    playPauseButton.classList.add('pause');
     videoBtn.style.display = 'block';
     }
 }
@@ -30,19 +30,17 @@ videoBtn.addEventListener('click', () => {
     splashScreen.style.display = 'none';
     videoPlayer.style.border = '1px solid #bdae82';
 });
-play.addEventListener('click', togglePlay);
-mute.addEventListener('click', () => {
-    if (!playerViewer.paused) {
-        if (playerViewer.muted) {
-            playerViewer.volume = playerVolume.value;
-            playerViewer.muted = false;
-            mute.classList.remove('active');
-        } else {
-            playerViewer.volume = 0.0;
-            playerViewer.muted = true;
-            mute.classList.add('active');
-        }
-    }    
+playPauseButton.addEventListener('click', togglePlay);
+muteButton.addEventListener('click', () => {
+    if (playerViewer.muted) {
+        playerViewer.volume = playerVolume.value;
+        playerViewer.muted = false;
+        muteButton.classList.remove('active');
+    } else {
+        playerViewer.volume = 0.0;
+        playerViewer.muted = true;
+        muteButton.classList.add('active');
+    }
 }); 
 playerVolume.addEventListener('input', () => {
     playerViewer.volume = playerVolume.value;
