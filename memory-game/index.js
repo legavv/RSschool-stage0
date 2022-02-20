3610f239b8e260bb99b67e8f08458a657688af79
Vadim Lega <legavv@mail.ru>
2022-02-21 02:15:51 +0300

    feat: add count of game step - with bag, don't restart

--- memory-game/index.js
+++ memory-game/index.js
@@ -4,10 +4,15 @@ const arrImgFace = [
 ]
 const wrapperCards = document.querySelector('.wrapper__cards');
 const url = './assets/img/'
+const countStep = document.querySelector('.count')
+let countStepGame = 0;
 let currentOpenImg = '';
 let countOpenCards = 0;
 
 showImages(mixRandomArray(arrImgFace));
+const img = document.querySelectorAll('.img')
+const cards = document.querySelectorAll('.card')
+addListenerToCards();
 
 function showImages (arrImages) {
     arrImages.forEach((el) => {
@@ -20,6 +25,7 @@ function showImages (arrImages) {
     img.setAttribute('alt', `${el}`)
     card.append(img)
     })
+    countStep.textContent = `${countStepGame}`;
 }
 function mixRandomArray (arr) {
     let randomArr = [];
@@ -40,9 +46,11 @@ function getRandomZeroInt (max) {
     return Math.floor( Math.random() * (max + 1) )
 }
 function findSameImg (event) {
+    countStepGame++
+    countStep.textContent = `${countStepGame}`;
     if (currentOpenImg) {
         if ( currentOpenImg === event.target.childNodes[0].alt ) { //the same alt card but not the same card 
-            cards.forEach((el) => {
+            cards.forEach((el, index) => {
                 if (el.className === 'card remove-bg') {
                     el.classList.remove('remove-bg')
                     el.classList.add('open')
@@ -63,22 +71,29 @@ function findSameImg (event) {
     }
     if (countOpenCards === arrImgFace.length) {
         countOpenCards = 0;
-        setTimeout((() => {
-            wrapperCards.innerHTML = '';
-            showImages(mixRandomArray(arrImgFace));
-        }), 2000)
-        
+        countStepGame = 0; 
+        setTimeout((() => {}), 2000) 
+        cards.forEach((el) => {
+            el.classList.remove('open')
+        }); 
+        img.forEach((el) => {
+            el.classList.remove('view')
+        })
+        wrapperCards.innerHTML = '';
+        showImages(mixRandomArray(arrImgFace));
+        addListenerToCards();
     }
 }
-
-const cards = document.querySelectorAll('.card')
-cards.forEach((el) => {
-    el.addEventListener('click', (e) => {
-        if (el.className !== 'card open' ) {
-            el.classList.add('remove-bg')
+function addListenerToCards () {
+    cards.forEach((el) => {
+        el.addEventListener('click', (e) => {
+            if (el.className !== 'card open' ) {
+                el.classList.add('remove-bg')
                 findSameImg(e)
-        }
+            }
+        })
     })
-})
+}
+
 
 
